10af3447eb8010b4d34b91a88a31e00f01fe5d9c
manuck <snc9000@naver.com>
2019-09-10 17:57:40 +0900

    [SM] | 190910 | A형_다리건설 수정

--- 모의/다리건설.py
+++ 모의/다리건설.py
@@ -108,7 +108,7 @@ def mst():
     D[u] = 0
 
     for i in range(V+1):  # 가중치 최소값 찾기
-        min = 987654321
+        min = 9999999
         for v in range(V+1):
             if visited[v] == 0 and min > D[v]:
                 min = D[v]
@@ -143,18 +143,13 @@ for case in range(t):
                 landlist.append(check(j, i))
                 landstart.append([j, i])
 
-    # print(landlist)
-    # for i in range(len(landlist)):
-    #     for j in range(n):
-    #         print(landlist[i][j])
-    #     print()
     landindex = list(range(len(landlist)))
     # print(landindex)
     sm = list(itertools.permutations(landindex, 2))
     myarray = [[0 for _ in range(len(landlist))]for _ in range(len(landlist))]
     # print(myarray)
     for i in range(len(sm)):
-        myarray[sm[i][1]][sm[i][0]] = vs(landlist[sm[i][0]], landlist[sm[i][1]], landstart[sm[i][0]])
+        myarray[sm[i][1]][sm[i][0]] = vs(landlist[sm[i][0]], landlist[sm[i][1]], landstart[sm[i][0]])   # 비교할 섬 A, B, 그리고 A의 시작 좌표
 
     print(myarray)
     zxc = []
@@ -165,9 +160,7 @@ for case in range(t):
 
     print(zxc)
     V, E = len(myarray)-1, len(myarray)
-    adj = [[0] * (V + 1) for _ in range(V + 1)] # myarray
-    D = [987654321] * (V + 1)
-    PI = list(range(V + 1))     # landindex
+    D = [99999] * (V + 1)
     visited = [0] * (V + 1)
 
     print(mst())

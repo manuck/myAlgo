e71d445a32fb82784e861716b8615d65f306dc18
manuck <snc9000@naver.com>
2019-09-20 17:12:48 +0900

    [SM] | 190920 | CodeForces | Perfect Team

--- CodeForces/Perfect Team.py
+++ CodeForces/Perfect Team.py
@@ -1,12 +1,11 @@
 import sys
 sys.stdin = open('Perfect Team_input.txt')
 
+t = int(input())
 
-# t = int(input())
-#
-# for case in range(t):
-#     m, c, x = map(int, input().split())
-#     all = m + c + x
-#     maxteam = all//3
-#     mc = min(m, c)
-#     print(min(mc, maxteam))
+for case in range(t):
+    m, c, x = map(int, input().split())
+    all = m + c + x
+    maxteam = all//3
+    mc = min(m, c)
+    print(min(mc, maxteam))

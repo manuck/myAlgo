234b8f9f392fec70d0a49bf2c278fc2d9b503d2f
manuck <snc9000@naver.com>
2019-09-20 09:31:48 +0900

    [SM] | 190920 | baekjoon | 14889_스타트와 링크

--- baekjoon/14889_스타트와 링크.py
+++ baekjoon/14889_스타트와 링크.py
@@ -5,14 +5,31 @@ import itertools
 
 n = int(input())
 g = [list(map(int, input().split()))for _ in range(n)]
-
-for i in range(n):
-    print(g[i])
-
 a = list(range(n))
 b = [0] * n
-print(a)
 c = list(itertools.combinations(a, n//2))
-print(c)
-for i in range(len(c)):
-    print(c[i])
+ans = 9999999
+
+for i in range(len(c)//2):
+    startPower = 0
+    linkPower = 0
+    # 스타트 팀 구분 및 시너지 계산
+    start = list(itertools.combinations(c[i], 2))
+    # print(start)
+    for j in range(len(start)):
+        startPower += g[start[j][0]][start[j][1]]
+        startPower += g[start[j][1]][start[j][0]]
+
+    # 링크 팀 구분 및 시너지 계산
+    link = list(itertools.combinations(c[-i-1], 2))
+    # print(link)
+    for j in range(len(link)):
+        linkPower += g[link[j][0]][link[j][1]]
+        linkPower += g[link[j][1]][link[j][0]]
+
+    # 스타트 팀과 링크 팀의 최소차이 계산
+    res = abs(startPower - linkPower)
+    if ans > res:
+        ans = res
+
+print(ans)

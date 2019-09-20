058ba945df2f472af324514dccf2bdcb0c88f623
manuck <snc9000@naver.com>
2019-09-20 17:10:57 +0900

    [SM] | 190920 | CodeForces | Knights

---
+++ CodeForces/Knights.py
@@ -0,0 +1,18 @@
+import sys
+sys.stdin = open('Knights_input.txt')
+
+n = int(input())
+g = [['W' for _ in range(n)]for _ in range(n)]
+
+# for i in range(n):
+#     print(g[i])
+
+for i in range(n):
+    for j in range(n):
+        if (i+j)%2 == 1:
+            g[i][j] = 'B'
+        else:
+            g[i][j] = 'W'
+
+for i in range(n):
+    print(''.join(g[i]))

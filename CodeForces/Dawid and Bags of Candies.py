1224d836dda8272a5742f69a7be2eed7dd93c503
manuck <snc9000@naver.com>
2019-09-24 10:32:24 +0900

    [SM] | 190924 | CodeForces | Dawid and Bags of Candies

---
+++ CodeForces/Dawid and Bags of Candies.py
@@ -0,0 +1,17 @@
+import sys
+sys.stdin = open('Dawid and Bags of Candies_input.txt')
+
+
+import itertools
+
+a = list(map(int, input().split()))
+b = list(itertools.combinations(a, 2))
+ans = 'NO'
+for i in range(len(b)//2):
+    if sum(b[i]) == sum(b[~i]):
+        ans = 'YES'
+
+if max(a) == sum(a)-max(a):
+    ans = 'YES'
+
+print(ans)

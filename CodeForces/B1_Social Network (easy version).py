22ea722874cb40a0f4f46206ec29502f435a3fad
manuck <snc9000@naver.com>
2019-10-02 00:55:46 +0900

    [SM] | 191002 | CodeForces | B1_Social Network(easy version)

--- CodeForces/B1_Social Network (easy version).py
+++ CodeForces/B1_Social Network (easy version).py
@@ -4,3 +4,17 @@ sys.stdin = open('B1_Social Network (easy version)_input.txt')
 n, k = map(int, input().split())
 
 a = list(map(int, input().split()))
+
+# print(n, k)
+# print(a)
+
+sol = []
+
+for i in range(len(a)):
+    if not a[i] in sol:
+        sol.insert(0, a[i])
+    if len(sol) > k:
+        sol.pop()
+print(len(sol))
+for i in range(len(sol)):
+    print(sol[i], end=" ")

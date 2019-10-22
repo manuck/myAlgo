f4e704b411b596a5403f7d30acea0e73e663ae30
manuck <snc9000@naver.com>
2019-10-23 01:46:12 +0900

    [SM] | 191023 | Baekjoon | 1922_네트워크 연결

--- baekjoon/1922_네트워크 연결.py
+++ baekjoon/1922_네트워크 연결.py
@@ -1,10 +1,6 @@
 import sys
 sys.stdin = open('1922_input.txt')
 
-<<<<<<< HEAD
-
-=======
->>>>>>> a7c87541b338e06a48eed1f7596c944b92cd96c7
 def find(x):
     if x != b[x]:
         b[x] = find(b[x])
@@ -29,16 +25,6 @@ m = int(input())
 
 a = []
 b = list(range(n+1))
-<<<<<<< HEAD
-for i in range(m):
-    a.append(list(map(int, input().split())))
-ans = 0
-cnt = 0
-# print(a)
-a.sort(key=lambda x: x[2])
-# print(a)
-# print(b)
-=======
 ans = 0
 cnt = 0
 
@@ -47,6 +33,5 @@ for i in range(m):
 
 a.sort(key=lambda x: x[2])
 
->>>>>>> a7c87541b338e06a48eed1f7596c944b92cd96c7
 mst()
 print(ans)

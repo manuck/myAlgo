7d7239500debf90ff79f46608e2f72247543966b
manuck <snc9000@naver.com>
2019-10-23 18:01:38 +0900

    [SM] | 191023 | Baekjoon | 1197_최소 스패닝 트리 풀이중

---
+++ baekjoon/1197_최소 스패닝 트리.py
@@ -0,0 +1,36 @@
+import sys
+sys.stdin = open('1197_input.txt')
+
+
+def find(x):
+    if x != b[x]:
+        b[x] = find(b[x])
+    return x
+
+def mst():
+    global ans, cnt
+    for a1, a2, a3 in a:
+        one = find(a1)
+        two = find(a2)
+        if one != two:
+            b[one] = two
+            ans += a3
+            cnt += 1
+        if cnt == v-1:
+            break
+
+
+v, e = map(int, input().split())
+a = []
+b = list(range(10001))
+ans = 0
+cnt = 0
+
+for _ in range(e):
+    a.append(list(map(int, input().split())))
+
+# print(a)
+a.sort(key=lambda x: x[2])
+print(a)
+mst()
+print(ans)

43bbaeebff30b434094eb7b70eeab3ffcbfc77ea
manuck <snc9000@naver.com>
2019-11-22 16:42:20 +0900

    [SM] | 191122 | Baekjoon | 1717 집합의 표현

---
+++ baekjoon/1717_집합의 표현.py
@@ -0,0 +1,32 @@
+import sys
+sys.stdin = open('1717_input.txt')
+
+input = sys.stdin.readline
+def find(num):
+    if p[num] != num:
+        p[num] = find(p[num])
+    return p[num]
+
+
+def union(a, b):
+    A = find(a)
+    B = find(b)
+    if A != B:
+        p[B] = A
+
+
+n, m = map(int, input().split())
+p = [i for i in range(n + 1)]
+
+for _ in range(m):
+    state, a, b = map(int, input().split())
+
+    if state:
+        if find(a) == find(b):
+            print("YES")
+
+        else:
+            print("NO")
+
+    else:
+        union(a, b)

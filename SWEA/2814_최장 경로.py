6f5a3884f4fc8d03440e97f594cf6be61432a4e3
manuck <snc9000@naver.com>
2019-11-25 17:53:07 +0900

    [SM] | 191125 | SWEA | 2814_최장 경로

---
+++ SWEA/2814_최장 경로.py
@@ -0,0 +1,30 @@
+import sys
+sys.stdin = open('2814_input.txt')
+
+def dfs(l, v):
+    global sol
+    visited[v] = 1
+    if sol < l:
+        sol = l
+    for w in range(1, n+1):
+        if g[v][w] == 1 and visited[w] == 0:
+            dfs(l + 1, w)
+    visited[v] = 0
+t = int(input())
+
+for case in range(t):
+    n, m = map(int, input().split())
+    visited = [0] * (n+1)
+    g = [[0 for _ in range(n+1)]for _ in range(n+1)]
+
+
+    for i in range(m):
+        x, y = map(int, input().split())
+        g[x][y] = 1
+        g[y][x] = 1
+
+    sol = 0
+    for i in range(n+1):
+        dfs(1, i)
+
+    print(f'#{case+1} {sol}')

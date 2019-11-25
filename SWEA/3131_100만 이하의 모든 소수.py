f7f7d0f668c32e25866768d1dfc3d989213804c2
manuck <snc9000@naver.com>
2019-11-25 15:56:38 +0900

    [SM] | 191125 | SWEA | 100만 이하의 모든 소수

---
+++ SWEA/3131_100만 이하의 모든 소수.py
@@ -0,0 +1,15 @@
+import sys
+sys.stdin = open('3131_input.txt')
+
+
+n = 1000000
+a = [False, False] + [True]*(n-1)
+primes = []
+
+for i in range(2, n+1):
+    if a[i]:
+        primes.append(i)
+        print(i, end=" ")
+        for j in range(2*i, n+1, i):
+            a[j] = False
+

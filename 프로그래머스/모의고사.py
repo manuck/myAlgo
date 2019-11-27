ae9d43b895540900958c21cd92fc79a335dbc5d8
manuck <snc9000@naver.com>
2019-11-27 17:15:02 +0900

    [SM] | 191127 | 프로그래머스 | 모의고사

--- 프로그래머스/모의고사.py
+++ 프로그래머스/모의고사.py
@@ -2,24 +2,21 @@ import sys
 sys.stdin = open('모의고사_input.txt')
 
 answers = list(map(int, input().split()))
-
 answer = []
-n = len(answers)
-# answers = answers * n
-a = [1,2,3,4,5] * 8
-b = [2,1,2,3,2,4,2,5] * 5
-c = [3,3,1,1,2,2,4,4,5,5] * 4
+a = [1, 2, 3, 4, 5]
+b = [2, 1, 2, 3, 2, 4, 2, 5]
+c = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
 a_cnt = 0
 b_cnt = 0
 c_cnt = 0
 for i in range(len(answers)):
-    if answers[i] == a[i]:
-        a_cnt+=1
-    if answers[i] == b[i]:
-        b_cnt+=1
-    if answers[i] == c[i]:
-        c_cnt+=1
-sol = [[1, a_cnt],[2, b_cnt],[3, c_cnt]]
+    if answers[i] == a[i % len(a)]:
+        a_cnt += 1
+    if answers[i] == b[i % len(b)]:
+        b_cnt += 1
+    if answers[i] == c[i % len(c)]:
+        c_cnt += 1
+sol = [[1, a_cnt], [2, b_cnt], [3, c_cnt]]
 sol.sort(key=lambda x: x[1], reverse=True)
 answer.append(sol[0][0])
 # print(sol)

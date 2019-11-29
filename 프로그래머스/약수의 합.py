e05893aa4d3c0aa19e2b7699e0d5073d5188d989
manuck <snc9000@naver.com>
2019-11-29 13:55:56 +0900

    [SM] | 191129 | 프로그래머스 | 약수의 합

--- 프로그래머스/약수의 합.py
+++ 프로그래머스/약수의 합.py
@@ -6,13 +6,16 @@ t = int(input())
 for case in range(t):
     n = int(input())
     answer = 0
-    visit = [1]*(n+1)
-    for i in range(1, n+1):
-        if visit[i]:
-            if n % i == 0:
-                visit[n//i] = 0
-                visit[i] = 0
-                answer += (n//i)
-                answer += i
+    # visit = [1]*(n+1)
+    # for i in range(1, n+1):
+    #     if visit[i]:
+    #         if n % i == 0:
+    #             visit[n//i] = 0
+    #             visit[i] = 0
+    #             answer += (n//i)
+    #             answer += i
 
+    for i in range(1, n+1):
+        if n % i == 0:
+            answer += i
     print(answer)

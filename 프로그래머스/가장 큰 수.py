248a19c27ba5f0f96dacb5f010e76cedc813d9a0
manuck <snc9000@naver.com>
2019-12-06 18:07:11 +0900

    [SM] | 191206 | 프로그래머스 | 가장 큰 수

--- 프로그래머스/가장 큰 수.py
+++ 프로그래머스/가장 큰 수.py
@@ -5,13 +5,22 @@ t = int(input())
 for case in range(t):
     numbers = list(map(int, input().split()))
     answer = ''
-    s = []
-    for number in numbers:
-        original = number
-        while number < 1000:
-            number *= 10
-        s.append([number, original])
-    # print(s)
-    s = sorted(s, key=lambda x: (-x[0], x[1]))
-    answer = str(int("".join([str(number[1]) for number in s])))
-    print(answer)
+    # 시간초과
+    # s = []
+    # for number in numbers:
+    #     original = number
+    #     while number < 1000:
+    #         number *= 10
+    #     s.append([number, original])
+    # # print(s)
+    # s = sorted(s, key=lambda x: (-x[0], x[1]))
+    # answer = str(int("".join([str(number[1]) for number in s])))
+
+    numbers = list(map(str, numbers))
+    print(numbers)
+    numbers.sort(key=lambda x: x*3, reverse=True)
+    print(numbers)
+    answer = str(int(''.join(numbers)))
+
+    # print(answer)
+    print()

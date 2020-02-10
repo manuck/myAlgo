045252df523064a54b728a979785438115737b74
manuck <snc9000@naver.com>
2020-02-11 00:21:00 +0900

    [SM] | 200211 | 프로그래머스 | 가장 큰 정사각형 찾기

--- 프로그래머스/가장 큰 정사각형 찾기.py
+++ 프로그래머스/가장 큰 정사각형 찾기.py
@@ -3,11 +3,22 @@ sys.stdin = open('가장 큰 정사각형 찾기_input.txt')
 
 t = int(input())
 for case in range(t):
-    n = int(input())
+    print()
+    K = int(input())
     board = []
     answer = 0
-    for i in range(n):
+    for i in range(K):
         board.append((list(map(int, input().split()))))
 
-    print(board)
-    
+    y = len(board)
+    x = len(board[0])
+    for i in range(y):
+        for j in range(x):
+            if board[i][j] == 1:
+                board[i][j] = min(board[i - 1][j - 1], min(board[i - 1][j], board[i][j - 1])) + 1
+            if answer < board[i][j]:
+                answer = board[i][j]
+    for i in range(y):
+        print(board[i])
+
+    print(answer ** 2)

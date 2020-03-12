21e1cb564bbc643927f20bc7454bd3ce102b5e30
manuck <snc9000@naver.com>
2020-03-12 12:18:27 +0900

    [SM] | 200312 | 프로그래머스 | 정수 삼각형

--- 프로그래머스/정수 삼각형.py
+++ 프로그래머스/정수 삼각형.py
@@ -23,3 +23,14 @@ for i in range(5):
 
 print(triangle)
 answer = 0
+
+for i in range(1, len(triangle)):
+    for j in range(i+1):
+        if j == 0:
+            triangle[i][j] += triangle[i-1][j]
+        elif j == i:
+            triangle[i][j] += triangle[i-1][j-1]
+        else:
+            triangle[i][j] += max(triangle[i-1][j], triangle[i-1][j-1])
+answer = max(triangle[-1])
+print(answer)

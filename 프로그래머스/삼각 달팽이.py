710a7a55c15c13ef5eea22185b90994e035ac194
manuck <snc9000@naver.com>
2020-11-06 23:34:23 +0900

    [SM] | 201106 | 프로그래머스 | 삼각 달팽이

---
+++ 프로그래머스/삼각 달팽이.py
@@ -0,0 +1,22 @@
+import sys
+sys.stdin = open('삼각 달팽이_input.txt')
+
+'''
+문제 설명
+정수 n이 매개변수로 주어집니다.
+ 다음 그림과 같이 밑변의 길이와 높이가 n인 삼각형에서 
+ 맨 위 꼭짓점부터 반시계 방향으로 달팽이 채우기를 진행한 후, 
+첫 행부터 마지막 행까지 모두 순서대로 합친 새로운 배열을 return 하도록 solution 함수를 완성해주세요.
+n은 1 이상 1,000 이하입니다.
+'''
+
+t = int(input())
+for case in range(t):
+    n = int(input())
+    answer = []
+
+    print(n)
+
+
+
+    print(answer)

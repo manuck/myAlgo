fcc6e245d50cd5c30812b7e0cfe2611b40d307f0
manuck <snc9000@naver.com>
2020-11-07 01:03:35 +0900

    [SM] | 201107 | 프로그래머스 | 삼각 달팽이

--- 프로그래머스/삼각 달팽이.py
+++ 프로그래머스/삼각 달팽이.py
@@ -14,8 +14,30 @@ t = int(input())
 for case in range(t):
     n = int(input())
     answer = []
-
+    g = [[0 for _ in range(n)] for _ in range(n)]
     print(n)
+    x, y = 0, -1
+    num = 1
+    for i in range(n):
+        for j in range(i, n):
+            if i % 3 == 0:
+                y += 1
+            elif i % 3 == 1:
+                x += 1
+            elif i % 3 == 2:
+                y -= 1
+                x -= 1
+            # print(num, y, x)
+            g[y][x] = num
+            num += 1
+            # print(g)
+    # for i in range(n):
+    #     print(g[i])
+    for i in range(n):
+        for j in range(n):
+            if g[i][j] != 0:
+                answer.append(g[i][j])
+
 
 
 

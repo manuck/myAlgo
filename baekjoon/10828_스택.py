cf8c47d9db84f3679213c07bf420d2d88663dff3
manuck <snc9000@naver.com>
2020-11-26 00:43:19 +0900

    [SM] | 201126 | Baekjoon | 10828_스택

--- baekjoon/10828_스택.py
+++ baekjoon/10828_스택.py
@@ -13,3 +13,34 @@ empty: 스택이 비어있으면 1, 아니면 0을 출력한다.
 top: 스택의 가장 위에 있는 정수를 출력한다. 만약 스택에 들어있는 정수가 없는 경우에는 -1을 출력한다.
 '''
 
+# 백준에서 input() 대신 sys.stdin.readline()을 사용해야 시간초과가 안뜸
+
+t = int(input())
+
+for case in range(t):
+    n = int(input())
+    stack = []
+    for _ in range(n):
+        a = list(map(str, input().split()))
+        # print(a, stack)
+        if len(a) == 2:
+            stack.append(a[1])
+        else:
+            if a[0] == 'pop':
+                if len(stack) > 0:
+                    print(stack.pop())
+                else:
+                    print(-1)
+            elif a[0] == 'size':
+                print(len(stack))
+            elif a[0] == 'empty':
+                if len(stack) == 0:
+                    print(1)
+                else:
+                    print(0)
+            elif a[0] == 'top':
+                if len(stack) > 0:
+                    print(stack[-1])
+                else:
+                    print(-1)
+    print()
